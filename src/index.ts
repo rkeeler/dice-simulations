2d7a99846be74e4522e271a96609042fcdb9e934
Reagan Keeler <6854684+rkeeler@users.noreply.github.com>
2019-02-03 15:43:22 -0700

    fixed typo

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ function main() {
   });
 
   logTable([
-    ['↓ Player | Enemy →', '1d4', '1d6', '1d8', '1d10,1d4', '1d12,1d6', '1d20,1d4'],
+    ['↓ Player | Enemy →', '1d4', '1d6', '1d8', '1d10,1d4', '1d12,1d6', '1d20,1d8'],
     createTableRow('1d4', [
       simulation.run([dice.d4], [dice.d4]),
       simulation.run([dice.d4], [dice.d6]),
